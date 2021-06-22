dbde31b766de3a503e6c7f3eeddb4a716006e098
Abdullah Al Mamoon <mamoon036@gmail.com>
2021-06-22 22:43:48 +0600

    22.06.2021

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php 
-function mamoon_enqueue_styles(){
-    wp_enqueue_style('style', get_stylesheet_uri(), [], fileatime( get_template_directory(). '/style.css' ));
+function mamoon_enqueue_scripts(){
+    wp_register_style('stylesheet', get_template_directory_uri(). '/assets/style.css', [], filemtime(get_template_directory(). '/assets/style.css'));
+    wp_enqueue_style('stylesheet');
 }
-add_action('wp_enqueue_scripts', 'mamoon_enqueue_styles');
+add_action('wp_enqueue_scripts', 'mamoon_enqueue_scripts');
