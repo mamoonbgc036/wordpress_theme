11c975cba44351ab9bbc7eff701e1eacc565475d
Abdullah Al Mamoon <mamoon036@gmail.com>
2021-07-15 00:23:21 +0600

    15.05.2021

--- inc/helpers/autoload.php
+++ inc/helpers/autoload.php
@@ -1,10 +1,15 @@
 <?php 
- namespace wordpress_theme\inc\helpers;
+ //namespace wordpress_theme\inc\helpers;
 
- function My_Autoloader($class){
-     //echo $class;die();
-     include_once $class.".php";
- }
+//  function My_Autoloader($class){
+//      include_once str_replace("wordpress_theme\\","",$class).".php";
+//  }
 
- spl_autoload_register("wordpress_theme\inc\helpers\My_Autoloader");
+//  spl_autoload_register("wordpress_theme\inc\helpers\My_Autoloader");
 
+// spl_autoload_register(function($class){
+//     $filePath = str_replace("wordpress_theme\\","",$class);
+//     include_once str_replace("\\","/",$filePath).".php";
+// });
+
+require_once "../inc/classes/wordpress_theme.php";
