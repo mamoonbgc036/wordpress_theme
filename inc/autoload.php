b43f44acebb4c3850dc1a2af1363e611a3d17c75
Abdullah Al Mamoon <mamoon036@gmail.com>
2021-07-17 23:57:10 +0600

    17.07.2021

--- inc/autoload.php
+++ inc/autoload.php
@@ -6,5 +6,5 @@ namespace wordpress_theme\inc\helpers;
 //     $filePath = str_replace("wordpress_theme\inc\\","",$class);
 //     include_once str_replace("\\","/",$filePath).".php";
 // });
-require_once '../classes/wordpress_theme.php';
+require_once 'classes/wordpress_theme.php';
 
--- index.php
+++ index.php
@@ -1,15 +1,4 @@
-<?php 
-echo "index";die();
-get_header(); 
-
-
-include_once "inc/helpers/autoload.php";
-
-
-new wordpress_theme\inc\classes\wordpress_theme();die();
-
-
-?>
+<?php get_header(); ?>
     <div class="content">
         hello wordpress
     </div>
