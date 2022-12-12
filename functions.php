3cad5402a7d2f8ef3ff142072b29c539a6d2f8b4
Abdullah Al Mamoon <mamoon036@gmail.com>
2022-12-12 22:11:29 +0600

    12.12.2022

--- functions.php
+++ functions.php
@@ -1,9 +1,19 @@
 <?php 
-/*
-Theme functions
-*/
 
-// Theme title
-add_theme_support('title-tag');
 
-?>
+function sharemind_style_and_scripts() {
+	wp_enqueue_style( 'base', get_template_directory_uri(). '/assets/css/base.css', null, '1.0', 'all' );
+	wp_enqueue_style( 'vendor', get_template_directory_uri(). '/assets/css/vendor.css', null, '1.0', 'all' );
+	wp_enqueue_style( 'main', get_template_directory_uri(). '/assets/css/main.css', null, '1.0', 'all' );
+	wp_enqueue_style( 'fonts', get_template_directory_uri(). '/assets/css/fonts.css', null, '1.0', 'all' );
+	wp_enqueue_style( 'font-awesome', get_template_directory_uri(). '/assets/css/font-awesome/font-awesome.min.css', null, '1.0', 'all' );
+	wp_enqueue_style( 'custom', get_stylesheet_uri(), null, '1.0', 'all' );
+
+	wp_enqueue_script( 'modernizer', get_theme_file_uri( 'assets/js/modernizr.js' ), null, '1.0' );
+	wp_enqueue_script( 'pace', get_theme_file_uri( 'assets/js/pace.min.js' ), null, '1.0' );
+
+	wp_enqueue_script( 'plugin', get_theme_file_uri( 'assets/js/plugins.js' ), array( 'jquery' ), '1.0', true );
+	wp_enqueue_script( 'main-js', get_theme_file_uri( 'assets/js/main.js' ), array( 'jquery' ), '1.0', true );
+}
+
+add_action( 'wp_enqueue_scripts', 'sharemind_style_and_scripts' );
